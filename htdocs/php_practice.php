33d1616c73d133dc78c8ac677385e3590e334a9a
Santos111111 <162523699+Santos111111@users.noreply.github.com>
2025-09-02 20:29:14 +0800

    Update php_practice.php

--- htdocs/php_practice.php
+++ htdocs/php_practice.php
@@ -144,91 +144,301 @@
 
 
  // Data Types
-    $a = "5 John";
-    $b = 5;
-    $c = 3.14;
-    $d = true;
-    $e = [1,2,3];
-    $f = new stdClass();
-    $g = null;
-    $h = fopen("php_practice.php", "r");
+    // $a = "John";
+    // $b = 5;
+    // $c = 3.14;
+    // $d = true;
+    // $e = [1,2,3];
+    // $f = new stdClass();
+    // $g = null;
+    // $h = fopen("php_practice.php", "r");
 
     //casting - string
-    // $a = (string) $a;
-    // $b = (string) $b;
-    // $c = (string) $c;
-    // $d = (string) $d;
-    // $e =  $e;
-    // $f =  $f;
-    // $g = (string) $g;
-    // $h = (string) $h;
 
-    // casting - int
-    // $a = (int) $a;
-    // $b = (int) $b;
-    // $c = (int) $c;
-    // $d = (int) $d;
-    // $e = (int) $e;
-    // $f =  $f;
-    // $g = (int) $g;
-    // $h = (int) $h;
+  // $a = (string) $a;
 
-    // casting- float
-    // $a = (float) $a;
-    // $b = (float) $b;
-    // $c = (float) $c;
-    // $d = (float) $d;
-    // $e = (float) $e;
-    // $f =  $f;
-    // $g = (float) $g;
-    // $h = (float) $h;
+  // $b = (string) $b;
 
-    // casting - boolean
-    // $a = (boolean) $a;
-    // $b = (boolean) $b;
-    // $c = (boolean) $c;
-    // $d = (boolean) $d;
-    // $e = (boolean) $e;
-    // $f =  $f;
-    // $g = (boolean) $g;
-    // $h = (boolean) $h;
+  // $c = (string) $c;
 
-    // casting-array
-    // $a = (array) $a;
-    // $b = (array) $b;
-    // $c = (array) $c;
-    // $d = (array) $d;
-    // $e = (array) $e;
-    // $f =  $f;
-    // $g = (array) $g;
-    // $h = (array) $h;
+  // $d = (string) $d;
 
-    // casting - unsent
-    $a = (null) $a;
-    $b = (null) $b;
-    $c = (null) $c;
-    $d = (null) $d;
-    $e = (null) $e;
-    $f =  $f;
-    $g = (null) $g;
-    $h = (null) $h;
+  // $e = $e;
 
-     var_dump($a);
-    echo "<br>";
-      var_dump($b);
-    echo "<br>";
-      var_dump($c);
-    echo "<br>";
-      var_dump($d);
-    echo "<br>";
-      var_dump($e);
-    echo "<br>";
-      var_dump($f);
-    echo "<br>";
-      var_dump($g);
-    echo "<br>";
-      var_dump($h);
-    echo "<br>";
+  // $f = $f;
+
+  // $g = (string) $g;
+
+  // $h = (string) $h;
+
+
+
+  // casting - int
+
+  // $a = (int) $a;
+
+  // $b = (int) $b;
+
+  // $c = (int) $c;
+
+  // $d = (int) $d;
+
+  // $e = (int) $e;
+
+  // $f = $f;
+
+  // $g = (int) $g;
+
+  // $h = (int) $h;
+
+
+
+  // casting- float
+
+  // $a = (float) $a;
+
+  // $b = (float) $b;
+
+  // $c = (float) $c;
+
+  // $d = (float) $d;
+
+  // $e = (float) $e;
+
+  // $f = $f;
+
+  // $g = (float) $g;
+
+  // $h = (float) $h;
+
+
+
+  // casting - boolean
+
+  // $a = (boolean) $a;
+
+  // $b = (boolean) $b;
+
+  // $c = (boolean) $c;
+
+  // $d = (boolean) $d;
+
+  // $e = (boolean) $e;
+
+  // $f = $f;
+
+  // $g = (boolean) $g;
+
+  // $h = (boolean) $h;
+
+
+
+  // casting-array
+
+  // $a = (array) $a;
+
+  // $b = (array) $b;
+
+  // $c = (array) $c;
+
+  // $d = (array) $d;
+
+  // $e = (array) $e;
+
+  // $f = $f;
+
+  // $g = (array) $g;
+
+  // $h = (array) $h;
+
+
+
+  // casting - unsent
+
+  // $a = (null) $a;
+
+  // $b = (null) $b;
+
+  // $c = (null) $c;
+
+  // $d = (null) $d;
+
+  // $e = (null) $e;
+
+  // $f = $f;
+
+  // $g = (null) $g;
+
+  // $h = (null) $h;
+
+
+
+  //  var_dump($a);
+
+  // echo "<br>";
+
+  //  var_dump($b);
+
+  // echo "<br>";
+
+  //  var_dump($c);
+
+  // echo "<br>";
+
+  //  var_dump($d);
+
+  // echo "<br>";
+
+  //  var_dump($e);
+
+  // echo "<br>";
+
+  //  var_dump($f);
+
+  // echo "<br>";
+
+  //  var_dump($g);
+
+  // echo "<br>";
+
+  //  var_dump($h);
+
+  // echo "<br>";
+
+  // php math
+  // $pi = pi();
+  // echo $pi,"<br>";
+
+  // $minNumber = min(300, 80,98,3);
+  // $maxNumber = max(300, 80,98,3);
+  // echo $minNumber,"<br>";
+  // echo $maxNumber,"<br>";
+  // //
+  // $num = -5;
+  // echo abs($num), "<br>";
+  // //
+  // $sqrt = sqrt(3);
+  // echo $sqrt,"<br>";
+  // //
+  // $rounded = round(3.49,1);
+  // echo $rounded,"<br>";
+  // //
+  // echo $rounded,"<br>";
+  // echo rand(),"<br>";
+
+  // echo rand(5,30);
+
+  //php constant
+//   define("_no1","hello",);
+//   define("_no2",25,);
+//   define("_no3",[1,2,3],);
+//   echo _no1, "<br>";
+//   echo _no2, "<br>";
+//  //echo _no3,"<br>";
+
+//   function test(){
+   
+//     echo _no1;
+//   }
+ 
+// test();
+ 
+//php operators
+//arik
+  //sum
+// echo 6 + 4 + 5, "<br>";
+//   //sub
+// echo 6 - 5 - 10,"<br>";
+// echo 6 - 2 + 10,"<br>"; //PEMDAS
+// //multi
+// echo 6 * 6 * 6,"<br>";
+// //div
+// echo 10 / 5,"<br>";
+// echo 6 + 6 * 2 / 2,"<br>"; //PEMDAS
+// // mod
+// echo 5 % 2,"<br>";
+// //expon **
+// echo 4 ** 2,"<br>";
+
+//assignment
+// $x = 5;
+// +=
+//-=
+//*=
+// /=
+// %=
+//$x = $sx + 10;
+// $x += 10;
+// echo $x
+
+//comparison
+// ==
+// var_dump(6 == "6");
+// echo "<br>";
+// // ===
+// var_dump(6 === "6");
+// echo "<br>";
+// // !=
+// var_dump(100 != 99);
+// echo "<br>";
+// // !==
+// var_dump(100 !== 99);
+// echo "<br>";
+// // >
+// // >=
+// //<=
+// var_dump(10 > 4);
+// echo "<br>";
+// var_dump(10 >= 4);
+// echo "<br>";
+// var_dump(10 <= 4);
+// echo "<br>";
+// // <=>
+// echo 10 <=>5;
+
+// //increment
+// $x = 5;
+// $y = 5;
+// //pre-incre
+// echo ++$x,"<br>";
+// //post-incre
+// echo $y++,"<br>";
+// echo $y,"<br>";
+
+//logical
+// and &&
+// var_dump(true and true);// true
+// echo "<br>";
+// var_dump(true and false);// false
+// echo "<br>";
+// var_dump(6 == "6" and 6 > 9); // fasle
+// echo "<br>";
+
+// // or ||
+// var_dump(false or false);// true
+// echo "<br>";
+// var_dump(6 == "6" || 6 > 9); // true
+// echo "<br>";
+
+// // xor
+// var_dump(true xor false); //true
+// echo "<br>";
+// var_dump(6 == "6" xor 6 > 9); // true
+// echo "<br>";
+
+// // !
+// var_dump(!true);
+// echo "<br>";
+
+// var_dump(!(6 == "6" xor 6 > 9)); // true
+// echo "<br>";
+
+// echo "Hello " . "World";
+// $x = "Hello";
+// $x = $x . "World";
+// $x .= "World";
+
+// echo $x;
 
 
     ?>
